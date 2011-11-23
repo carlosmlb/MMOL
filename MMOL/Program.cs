d300290e620fe6c7af820939b921079e7135aa4a
Sergio PR <sergio.padrino@gmail.com>
2011-11-23 14:09:21 +0100

    Add GPL license text

--- MMOL/Program.cs
+++ MMOL/Program.cs
@@ -1,4 +1,22 @@
-﻿// Need to add the following References if using VC# 2008:
+﻿/**
+ *  MMOL Project: API Tool for increase client functionalities of
+ *  realXtend technologies.
+ *  Copyright (C) 2010 Information Engineering Research Unit
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * */
+// Need to add the following References if using VC# 2008:
 // OpenMetaverse, OpenMetaverseTypes, System
 // These can be found in your opensim\bin folder
 using System;
